0ee4c8868a461eac012201d6caf0a281320792fa
matao <matao@tdgcore.com>
2025-09-22 15:28:24 +0800

    Initial commit

---
+++ src/main/java/com/example/mylearningrepo/MyLearningRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mylearningrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLearningRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLearningRepoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mylearningrepo/MyLearningRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mylearningrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLearningRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
